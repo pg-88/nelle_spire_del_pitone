3b731c84e1b0d51fe09a4158d06425a90c78bb61
libero.bo <libero.bo@formart.it>
2023-02-28 13:51:58 +0100

    primo commit

--- src/files_py/funzioni_pitone.py
+++ src/files_py/funzioni_pitone.py
@@ -31,3 +31,11 @@ print("v è una variabile il cui valore è: ", v, '\n', "mentre la variabile quo
 #             ^stringa costante                   ^singolo carattere(carattere speciale)
 
 ##### Per sperimentare si potrebbe creare una lista di nomi e una lista di età e provare a selezionarli e comporli per fare una stringa
+names = ["Pluto","Balto","Lessy","Pongo"]
+age =  [121,15,34,12]
+print ("Il cane", names[0], "ha",str(age[0]), "anni",'\n' "Il cane", names[1], "ha",str(age[1]), "anni,""\n"
+       "Il cane", names[2], "ha",str(age[2]), "anni, Il cane", names[3], "ha",str(age[3]),"anni.")
+
+#provo a stampare tutti i cani con relativi anni innescando un ciclo
+#devo prima contare il numero di elementi della lista, poi posso innescare un ciclo con numero di cicli pari al numero degli elementi e stampare 
+# gli elementi delle due liste con posizione della lista pari al numero del ciclo. Nota : cosa succede se le liste non hanno lo stesso numero di elementi?
