86774c5cc59aed2f87d25cb59586efdcc28bd913
Davide <email@gmail.com>
2023-03-03 13:01:29 +0100

    Controllo su inpunt s/n sul nome inserito

--- src/files_py/playground.py
+++ src/files_py/playground.py
@@ -5,16 +5,23 @@ nome
 print(f'il nome inserito è : {nome}, è correttò?')
 
 checknome = input('s/n ? : ')
-checknome
+
+
+while (checknome !="n" and checknome != "s"):
+   print ("ops! Non ho capito, insierisci la lettera s se il nome è giusto o n se vuoi cambiare nome ")
+   
+   print(f'il nome inserito è : {nome}, è correttò?')
+
+   checknome = input('s/n ? : ')
 
 while checknome == "n":
    print ("ops! Riproviamo... inserisci il tuo nome ")
    nome = input ('inserisci il tuo nome : ')
-   nome
+   
    print(f'il nome inserito è : {nome}, è correttò?')
 
    checknome = input('s/n ? : ')
-   checknome
+   
 
 
 print(f"ok {nome}, ti va di fare un gioco, si chiama indovina numero \ns/n?")
@@ -24,7 +31,7 @@ check_play= input(" ")
 while check_play == "s":
 
    numero_utente = input ('Benissimo! Prova a indovinare a cosa sto pensando, inserisci un numero da 1 a 5 : ')
-   numero_utente
+   
 
    import random
    numero_intero_casuale = random.randint(1,5)
@@ -39,7 +46,7 @@ while check_play == "s":
 
       tentativo=tentativo+1
       numero_utente = input ('inserisci un numero : ')
-      numero_utente
+      
 
 
    tentativo = tentativo+1
