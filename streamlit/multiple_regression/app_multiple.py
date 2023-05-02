6e388d76a162f647e0116ccac55d4ba5621cadb6
Pietro Grigolo <grigolopi@gmail.com>
2023-05-02 10:55:23 +0200

    appunti prime ore

--- streamlit/multiple_regression/app_multiple.py
+++ streamlit/multiple_regression/app_multiple.py
@@ -6,12 +6,21 @@ import pandas as pd
 from modules.multiple import MultipleRegression as mr
 
 
+
 def main():
     path = "https://frenzy86.s3.eu-west-2.amazonaws.com/python/data/Startup.csv"
-    df = pd.read_csv(path)
-    st.write(df)
-    reg = mr(df, "Profit")
-
+    st.markdown(
+        """## WHERE ARE THE DATAAA??? 
+        
+        Do you have a .csv or a link?""")
+    link = st.text_input(label="") # input del link o del path di sis del file csv
+    in_file = st.file_uploader(label="Gimmi da file...", type="csv")
+    target = st.text_input(label="Write here the name of the target column")
+    if st.button("create the model"):
+        if(in_file != None and target != None):
+            pass # creare il modello con la classe Multiple regression usando il file csv
+        elif(link != None and target != None):
+            pass # 
 
 if __name__ == "__main__":
     main()
